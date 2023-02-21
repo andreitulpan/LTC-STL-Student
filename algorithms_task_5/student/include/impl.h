3e61073b3dbf8f84ff0211d04a2f69e4cb29ec2c
andreitulpan <andrei.tulpan2@gmail.com>
2023-02-21 17:50:17 +0200

    algorithms_task_5 done

--- algorithms_task_5/student/include/impl.h
+++ algorithms_task_5/student/include/impl.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <algorithm>
+#include <functional>
 
 /**
  * @todo Implement function that will bring all values satisfying predicate to specified position
@@ -10,4 +11,8 @@
  * @return gathered range
  */
 template <typename BiIt, typename UnaryPredicate>
-std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate);
+std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate) {
+    auto first = std::partition(begin, position, [&predicate](typename BiIt::value_type value) { return !predicate(value); });
+    auto last = std::partition(position, end, predicate);
+    return std::make_pair(first, last);
+}
